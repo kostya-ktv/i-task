0b32d6009c1ed708c973280cf46e51c631b8b0cb
Kostya <54177203+kostya-ktv@users.noreply.github.com>
2024-02-05 11:51:02 +0200

    [add] get cards api & fetcher

---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { cardId: string } }
+) {
+  try {
+    const { userId, orgId } = auth();
+    if (!userId || !orgId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!params.cardId) {
+      return new NextResponse("Invalid id provided", { status: 500 });
+    }
+
+    const card = await db.card.findUniqueOrThrow({
+      where: {
+        id: params.cardId,
+        list: {
+          board: { orgId },
+        },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(card);
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
